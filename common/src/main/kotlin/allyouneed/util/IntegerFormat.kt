f5d3adb6dff156e9fc2da53513e2edb281418fa8
copi143 <copi143@outlook.com>
2026-08-01 00:16:01 +0800

    整数格式化 (TODO)

---
+++ common/src/main/kotlin/allyouneed/util/IntegerFormat.kt
@@ -0,0 +1,80 @@
+package allyouneed.util
+
+import java.math.BigInteger
+
+data class IntegerFormat(
+    // 每个单位的基数，一般是 1000 或 1024
+    val base: Int,
+    // 每级单位的后缀，从无缩放开始，类似 ["", "k", "M", "G"]
+    val postfixes: List<String>,
+    // 目标文本长度，输出可以比其短但不能比其长，0 为无限制
+    val width: Int = 0,
+    // 最小值，当数字小于该值时，显示为 minDisplay
+    val min: BigInteger? = null,
+    // 最大值，当数字大于该值时，显示为 maxDisplay
+    val max: BigInteger? = null,
+    // 最小值显示的替代文本
+    val minDisplay: String = "MIN",
+    // 最大值显示的替代文本
+    val maxDisplay: String = "MAX",
+    // 允许负数（不允许时负数会被显示为 errDisplay）
+    // - 显示负数时会先将 width 减 1 后格式化绝对值，最后添加上负号
+    val allowNegative: Boolean = false,
+    // 在正整数前添加正号，格式化输出类似 -1 0 +1
+    // - 与负数相同，会先将 width 减 1 后格式化绝对值，最后添加上正号
+    val showPositiveSign: Boolean = false,
+    // 数值格式化错误时显示的内容（格式化错误可能是因为宽度不够）
+    val errDisplay: String = "ERR",
+    // 超过最大单位时是否允许使用 +1 +2 的形式表示，每 +1 就是乘一次 base
+    // - 开启后：最大单位设置为 G 时 1T 会被表示为 1G+1
+    // - 不开启：最大单位设置为 G 时 1T 会被表示为 1000G
+    // - 如果设置的 max 值不够大就不会出现超过最大单位被提升而是直接变成 maxDisplay
+    // - +k 的 k 没有限制，除非出现 1G+100 但最大宽度设定为 5 导致输出 errDisplay
+    val allowPlus: Boolean = false,
+    // 进入从当前级别进入下一级单位的阈值
+    // - 设置为 500 时 500M 会显示为 0.5G
+    // - 设置为 1500 是 1000M 仍然显示为 1000M，直到 1500M 才显示为 1.5G
+    // - 0 表示大于等于基数时提升一级
+    // - 每次提升时将数字除以 base 然后与 threshold 判定是否需要再次提升
+    // - allowPlus 状态下可以无限提升，否则只能提升到最大单位
+    // - 如果 format 后出现长度不符合 width 则再尝试提升一级，如果再次失败则直接输出 errDisplay
+    val threshold: Int = 0,
+    // 是否允许省略为 0 的小数，类似 1.00E -> 1E 或 2.30M -> 2.3M
+    val allowOmitDecimal: Boolean = false,
+    // 是否允许小数无前缀 0，类似 0.3G -> .3G
+    val allowOmitLeadingZero: Boolean = false,
+    // 最长小数位数，默认为自动：round(log10(base))
+    val maxDecimalPlaces: Int = -1,
+    // 舍入模式，永远对绝对值进行舍入，不考虑符号
+    val rounding: Rounding = Rounding.HalfUp,
+) {
+    enum class Rounding {
+        Up, Down, HalfUp, HalfDown, HalfEven
+    }
+
+    init {
+        if (width > 0) {
+            if (min != null && minDisplay.length > width) {
+                throw IllegalArgumentException("minDisplay length exceeds width")
+            }
+            if (max != null && maxDisplay.length > width) {
+                throw IllegalArgumentException("maxDisplay length exceeds width")
+            }
+            if (errDisplay.length > width) {
+                throw IllegalArgumentException("errDisplay length exceeds width")
+            }
+        }
+    }
+
+    fun format(value: BigInteger): String {
+        TODO()
+    }
+
+    fun format(value: Long): String {
+        TODO()
+    }
+
+    fun format(value: Int): String {
+        TODO()
+    }
+}
