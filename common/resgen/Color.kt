3790cadfda2ad126636ece928c80f4b19b8ffdd1
copi143 <copi143@outlook.com>
2026-07-31 20:20:51 +0800

    添加渐变

--- common/resgen/Color.kt
+++ common/resgen/Color.kt
@@ -29,9 +29,27 @@ val AE2_COLORS = run {
     array
 }
 
+val AE2_GRADIENT = run {
+    val array = arrayListOf<Color>()
+    for (i in 0..19) {
+        val newColor = gamutMap(
+            JzCzHz(j = 0.01f, c = 0.01f, h = 240f - i * 18)
+        ).toSRGB()
+        val r = (newColor.r * 255).roundToInt()
+        val g = (newColor.g * 255).roundToInt()
+        val b = (newColor.b * 255).roundToInt()
+        array.add(Color(r, g, b))
+    }
+    array
+}
+
 fun main() {
-    println("AE2 Colors:")
+    println("\nAE2 Colors:")
     for ((index, color) in AE2_COLORS.withIndex()) {
         println("Color $index: ${color.hex} ${color.termFg}Sample Text\u001b[m ${color.termBg}Sample Background\u001b[m")
+    }
+    println("\nAE2 Gradient:")
+    for ((index, color) in AE2_GRADIENT.withIndex()) {
+        println("Gradient $index: ${color.hex} ${color.termFg}Sample Text\u001b[m ${color.termBg}Sample Background\u001b[m")
     }
 }
