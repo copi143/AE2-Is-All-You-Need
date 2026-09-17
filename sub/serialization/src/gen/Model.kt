867b025476ec0291a157fba76ea8edb244e06013
copi143 <copi143@outlook.com>
2026-09-17 14:21:50 +0800

    修改 SerialTy

--- sub/serialization/src/gen/Model.kt
+++ sub/serialization/src/gen/Model.kt
@@ -17,79 +17,144 @@ data class SerialProp(
     val varLen: Boolean = false,
 )
 
-sealed class SerialTy(vararg types: String) {
-    open val type: String = types.firstOrNull() ?: ""
-    open val shortType: String = types.firstOrNull()?.removePrefix("kotlin.")?.removePrefix("java.lang.") ?: ""
-    open val types: Set<String> = types.toSet()
+sealed class SerialTy(
+    val type: String,
+    val shortType: String,
+    val types: Set<String>,
+    open val nbt: Pair<String, String>,
+    open val buf: Pair<String, String>,
+) {
+    constructor(
+        types: List<String>,
+        nbt: Pair<String, String>,
+        buf: Pair<String, String>,
+    ) : this(
+        types.first().removePrefix("#"),
+        types.first().removePrefix("#").removePrefix("kotlin.").removePrefix("java.lang."),
+        types.filter { !it.startsWith("#") }.toSet(),
+        nbt,
+        buf,
+    )
 
-    data object Bool : SerialTy("kotlin.Boolean", "java.lang.Boolean")
+    constructor(
+        type: String,
+        nbt: Pair<String, String>,
+        buf: Pair<String, String>,
+    ) : this(listOf(type), nbt, buf)
 
-    data object I8 : SerialTy("kotlin.Byte", "java.lang.Byte")
-    data object I16 : SerialTy("kotlin.Short", "java.lang.Short")
-    data object I32 : SerialTy("kotlin.Int", "java.lang.Integer")
-    data object I64 : SerialTy("kotlin.Long", "java.lang.Long")
-    data object VarI32 : SerialTy()
-    data object VarI64 : SerialTy()
+    constructor(type: String, java: Boolean = true) : this(
+        if ('.' !in type) {
+            if (java) listOf("kotlin.$type", "java.lang.$type") else listOf("kotlin.$type")
+        } else {
+            listOf(type)
+        },
+        "get$type" to "put$type",
+        "read$type" to "write$type",
+    )
 
-    data object U8 : SerialTy("kotlin.UByte")
-    data object U16 : SerialTy("kotlin.UShort")
-    data object U32 : SerialTy("kotlin.UInt")
-    data object U64 : SerialTy("kotlin.ULong")
-    data object VarU32 : SerialTy()
-    data object VarU64 : SerialTy()
+    val nbtGet: String get() = nbt.first
+    val nbtSet: String get() = nbt.second
+    val bufGet: String get() = buf.first
+    val bufSet: String get() = buf.second
 
-    data object BigInt : SerialTy("java.math.BigInteger")
-    data object VarBigInt : SerialTy()
+    sealed class Number(val bits: Int, val unsigned: Boolean, val varLen: Boolean = false) : SerialTy(
+        types(bits, unsigned).map { if (varLen) "#$it" else it },
+        "get${suffix(bits)}" to "put${suffix(bits)}",
+        "read${suffix(bits)}" to "write${suffix(bits)}",
+    ) {
+        companion object {
+            fun suffix(bits: Int) = when (bits) {
+                8 -> "Byte"
+                16 -> "Short"
+                32 -> "Int"
+                64 -> "Long"
+                else -> throw IllegalArgumentException()
+            }
 
-    data object F32 : SerialTy("kotlin.Float", "java.lang.Float")
-    data object F64 : SerialTy("kotlin.Double", "java.lang.Double")
+            fun types(bits: Int, unsigned: Boolean): List<String> {
+                val suffix = suffix(bits)
+                return if (unsigned) {
+                    listOf("kotlin.U$suffix")
+                } else {
+                    listOf("kotlin.$suffix", "java.lang.$suffix")
+                }
+            }
+        }
+    }
 
-    data object Str : SerialTy("kotlin.String", "java.lang.String")
-    data object I8Array : SerialTy("kotlin.ByteArray")
-    data object I32Array : SerialTy("kotlin.IntArray")
-    data object I64Array : SerialTy("kotlin.LongArray")
+    data object Bool : SerialTy("Boolean")
+    data object F32 : SerialTy("Float")
+    data object F64 : SerialTy("Double")
+    data object Str : SerialTy("String") {
+        override val buf = "readUtf" to "writeUtf"
+    }
+
+    data object I8 : Number(8, false)
+    data object I16 : Number(16, false)
+    data object I32 : Number(32, false)
+    data object I64 : Number(64, false)
+    data object VarI32 : Number(32, false, varLen = true)
+    data object VarI64 : Number(64, false, varLen = true)
+
+    data object U8 : Number(8, true)
+    data object U16 : Number(16, true)
+    data object U32 : Number(32, true)
+    data object U64 : Number(64, true)
+    data object VarU32 : Number(32, true, varLen = true)
+    data object VarU64 : Number(64, true, varLen = true)
+
+    data object BigInt : SerialTy("java.math.BigInteger", nbtByteArray, bufByteArray)
+    data object VarBigInt : SerialTy("#java.math.BigInteger", nbtByteArray, bufByteArray)
+
+    data object I8Array : SerialTy("ByteArray", false)
+    data object I32Array : SerialTy("IntArray", false)
+    data object I64Array : SerialTy("LongArray", false)
+
     data object UUID : SerialTy("java.util.UUID")
     data object ResLoc : SerialTy("net.minecraft.resources.ResourceLocation")
     data object BlockPos : SerialTy("net.minecraft.core.BlockPos")
 
-    data class Enum(val name: String, val ordinal: Boolean) : SerialTy() {
-        override val type: String = name
-        override val shortType: String = name
-        override val types: Set<String> = setOf(type)
-    }
+    data class Enum(val name: String, val ordinal: Boolean) : SerialTy(
+        name,
+        if (ordinal) I8.nbt else Str.nbt,
+        if (ordinal) I8.buf else Str.buf,
+    )
 
-    data class ListOf(val element: SerialTy) : SerialTy() {
-        override val type: String = "kotlin.collections.List<${element.type}>"
-        override val shortType: String = "List<${element.shortType}>"
-        override val types: Set<String> = setOf(type)
-    }
+    data class ListOf(val element: SerialTy) : SerialTy(
+        "kotlin.collections.List<${element.type}>",
+        "List<${element.shortType}>",
+        setOf("kotlin.collections.List<${element.type}>"),
+        "get" to "put",
+        "" to "",
+    )
 
-    data class MapOf(val key: SerialTy, val value: SerialTy) : SerialTy() {
-        override val type: String = "kotlin.collections.Map<${key.type}, ${value.type}>"
-        override val shortType: String = "Map<${key.shortType}, ${value.shortType}>"
-        override val types: Set<String> = setOf(type)
-    }
+    data class MapOf(val key: SerialTy, val value: SerialTy) : SerialTy(
+        "kotlin.collections.Map<${key.type}, ${value.type}>",
+        "Map<${key.shortType}, ${value.shortType}>",
+        setOf("kotlin.collections.Map<${key.type}, ${value.type}>"),
+        "get" to "put",
+        "" to "",
+    )
 
-    data class SetOf(val element: SerialTy) : SerialTy() {
-        override val type: String = "kotlin.collections.Set<${element.type}>"
-        override val shortType: String = "Set<${element.shortType}>"
-        override val types: Set<String> = setOf(type)
-    }
+    data class SetOf(val element: SerialTy) : SerialTy(
+        "kotlin.collections.Set<${element.type}>",
+        "Set<${element.shortType}>",
+        setOf("kotlin.collections.Set<${element.type}>"),
+        "get" to "put",
+        "" to "",
+    )
 
-    data class Nested(val name: String) : SerialTy() {
-        override val type: String = name
-        override val shortType: String = name
-        override val types: Set<String> = setOf(type)
-    }
+    data class Nested(val name: String) : SerialTy(name, name, setOf(name), "" to "", "" to "")
 
     companion object {
+        private val nbtByteArray = "getByteArray" to "putByteArray"
+        private val bufByteArray = "readByteArray" to "writeByteArray"
+
         private val primitives: List<SerialTy> = listOf(
-            Bool,
+            Bool, F32, F64, Str,
             I8, I16, I32, I64, VarI32, VarI64,
             U8, U16, U32, U64, VarU32, VarU64,
             BigInt, VarBigInt,
-            F32, F64,
-            Str,
             I8Array, I32Array, I64Array,
             UUID, ResLoc, BlockPos,
         )
