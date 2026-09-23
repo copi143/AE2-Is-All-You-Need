f1d3fd4af048f49f6a7d37ac38427472a6c2050a
copi143 <copi143@outlook.com>
2026-09-23 20:37:57 +0800

    更新依赖

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -13,8 +13,8 @@ pluginManagement {
         }
     }
     plugins {
-        kotlin("jvm") version "2.4.10"
-        kotlin("kapt") version "2.4.10"
+        kotlin("jvm") version "2.4.20"
+        kotlin("kapt") version "2.4.20"
     }
 }
 
